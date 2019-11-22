40940bb4e716428760485c9e7618be20a45b77cf
Garfield Lee <openpgp@550.moe>
2019-11-22 15:20:09 +0800

    feat: setup basic development environment

--- src/components/image/index.tsx
+++ src/components/image/index.tsx
@@ -1,6 +1,6 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img, { FluidObject } from 'gatsby-image';
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -13,8 +13,16 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => {
-  const data = useStaticQuery(graphql`
+interface QueryData {
+  placeholderImage: {
+    childImageSharp: {
+      fluid: FluidObject;
+    };
+  };
+}
+
+const Image: React.FC = (): JSX.Element => {
+  const data: QueryData = useStaticQuery(graphql`
     query {
       placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
         childImageSharp {
@@ -24,9 +32,9 @@ const Image = () => {
         }
       }
     }
-  `)
+  `);
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-}
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />;
+};
 
-export default Image
+export default Image;
---
+++ src/components/seo/index.tsx
@@ -0,0 +1,98 @@
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+
+export interface SeoProps {
+  description?: string;
+  lang?: string;
+  meta?: React.DetailedHTMLProps<
+    React.MetaHTMLAttributes<HTMLMetaElement>,
+    HTMLMetaElement
+  >[];
+  title: string;
+}
+
+interface QueryData {
+  site: {
+    siteMetadata: {
+      title: string;
+      description: string;
+      author: string;
+    };
+  };
+}
+
+const SEO: React.FC<SeoProps> = ({
+  title,
+  description = '',
+  lang = 'en',
+  meta = [],
+}): JSX.Element => {
+  const { site }: QueryData = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  );
+
+  const metaDescription = description || site.siteMetadata.description;
+  const extraMetaInfo: React.DetailedHTMLProps<
+    React.MetaHTMLAttributes<HTMLMetaElement>,
+    HTMLMetaElement
+  >[] = [
+    {
+      name: `description`,
+      content: metaDescription,
+    },
+    {
+      property: `og:title`,
+      content: title,
+    },
+    {
+      property: `og:description`,
+      content: metaDescription,
+    },
+    {
+      property: `og:type`,
+      content: `website`,
+    },
+  ];
+  const metaInfo = extraMetaInfo.concat(meta);
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang: lang || 'en',
+      }}
+      title={title}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      meta={metaInfo}
+    />
+  );
+};
+
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
+// @ts-ignore
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+};
+
+export default SEO;
