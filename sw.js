4a0a2490eb5b96f9a29f7d63d3fadebcc9fa78d0
Garfield550 <Garfield550@users.noreply.github.com>
2020-04-03 21:30:37 +0000

    Automated deployment: Fri Apr  3 21:30:37 UTC 2020 d65473280e904020f85dd6be582dc50c69f95dce

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0f01b15e982f515614f31ed22f7f161d"
+    "revision": "b57a7fd6beb8c3050feb65b5b7f7bdbb"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-32da291822574515e9a8.js"
